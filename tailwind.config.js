fd2c5fdc8d15b23e64e772f0544ce25ff933c178
david dong <hi@daviddong.me>
2023-12-13 22:18:45 +0800

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
-  plugins: [require("daisyui"),require('tailwindcss-animate')]
+  plugins: [require("daisyui")]
 }
